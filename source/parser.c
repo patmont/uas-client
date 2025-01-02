7e0d085b98c03bfa248e0d50a063662e131c20e6
patmont <pat@patmont.net>
2025-01-02 08:50:07 -0800

    First build

--- source/parser.c
+++ source/parser.c
@@ -3,6 +3,13 @@
 #include "opendroneid.h"
 #include "database.h"
 
+uint8_t* extract_payload(const uint8_t *packet) {
+    const size_t header_len = 122;
+    const uint8_t *payload = packet + header_len;
+
+    return (uint8_t *)payload;
+}
+
 void parse_packet(const unsigned char *packet) {
     ODID_UAS_Data uasData;
     uint8_t *payload = extract_payload(packet);
